bfb06a2176ed1be53bd07b603eb4774656db05fa
jiangsit <1319791967@qq.com>
2017-08-21 15:20:05 +0800

    Update 14. Longest Common Prefix.cpp

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -16,3 +16,26 @@ public:
         return prefix;    
     }
 };
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        if (n == 0) {
+            return "";
+        }
+        string res = strs[0];
+        int i, j;
+        int len;
+        for (i = 1; i < n; ++i) {
+            len = res.size();
+            for (j = 0; j < len; ++j) {
+                if (strs[i][j] != res[j]) {
+                    res.resize(j);//重新截取字符串大小
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
